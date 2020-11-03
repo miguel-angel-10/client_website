b257980fb84d79e9375dc271a682577c5f1afbaa
sanaa <s.rortas@codeur.online>
2020-11-03 11:50:18 +0100

    modification header footer

--- index.php
+++ index.php
@@ -1,8 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>accueil</title>
+  <link rel="stylesheet" href="css/clientjardin.css">
+</head>
+
 
   <?php include( 'templates/header.php' ); ?>
+  
+<body>
 
+  
+
+
+  <label for="s'identifier"></label>
+  <input type="button" value="s'identifier">
+
+  
+</body> 
+
+
+
+ 
  
 
 
