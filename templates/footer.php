8f89fc607e089f80e332ee622db1dfa3cffaf897
Miguel <m.urcia@codeur.online>
2020-11-03 09:37:45 +0100

    html structure base, templates

---
+++ templates/footer.php
@@ -0,0 +1,20 @@
+<!-- Footer include page -->
+
+<?php
+
+  ?>
+
+<footer>
+    <div class="footer-section">
+      <ul>
+        <li class="footer-lien"><a href="#">lien</a></li>
+        <li class="footer-lien"><a href="#"></a></li>
+        <li class="footer-lien"><a href="#"></a></li>
+        <li class="footer-lien"><a href="#"></a></li>
+        <li class="footer-lien"><a href="#"></a></li>
+      </ul>
+    </div>
+  </footer>
+
+  
+</body>
---
+++ templates/header.php
@@ -0,0 +1,19 @@
+<!-- Header include page -->
+
+<?php
+
+?>
+
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Accueil</title>
+</head>
+<body>
+
+<header>
+  <nav>
+    <h2>header nav</h2>
+  </nav>
+</header>
+
