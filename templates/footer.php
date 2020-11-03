ecb51facf5ded2d4537601bc758d6c4f222d684f
sanaa <s.rortas@codeur.online>
2020-11-03 17:47:25 +0100

    ajout images et  css

--- templates/footer.php
+++ templates/footer.php
@@ -7,7 +7,7 @@
 <footer>
              <h4> contactez-nous </h4>
     <div class="footer-section">
-      <ul>
+      <ul class="footerbox">
         <li class="footer-lien"><a href="#">lien</a></li>
         <li class="footer-lien"><a href="#">lien</a></li>
         <li class="footer-lien"><a href="#">lien</a></li>
--- templates/header.php
+++ templates/header.php
@@ -7,14 +7,16 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="assets/css/clientjardin.css">
 <title>Accueil</title>
+
 </head>
 <body>
 
 <header>
   <nav>
   
-    <h2>Bienveunue chez M&co</h2>
+    <h2>Bienvenue chez M&co</h2>
     
     <ul>
       <li>
@@ -24,7 +26,9 @@
         <a href="formulaire.php">s'identifier</a> 
       </li>
     </ul>
-  
+
   </nav>
 </header>
+
+
 
